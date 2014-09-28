a4647b345b98e01ea989d7dc6c316db0226f0bd1
Geekii <simplykk@users.noreply.github.com>
2014-09-28 16:14:32 +0800

    first iOS demo

---
+++ FirstIOS/FirstIOS/AppDelegate.m
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.m
+//  FirstIOS
+//
+//  Created by Geekii on 14-9-28.
+//  Copyright (c) 2014年 Geekii. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    NSLog(@"didFinishLaunchingWithOptions");
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"applicationWillResignActive");
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"applicationDidEnterBackground");
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"applicationWillEnterForeground");
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"applicationDidBecomeActive");
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"applicationWillTerminate");
+    // 这个方法不一定调用，如果程序已经进入后台，再关闭改程序，则不会调用这个事件。但是如果程序在前台被关闭，则会进入这个事件。
+}
+
+@end
---
+++ FirstIOS/FirstIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstIOS
+//
+//  Created by Geekii on 14-9-28.
+//  Copyright (c) 2014年 Geekii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
